413b2a8ae99ab7908a23d2f3a471a197d31fd126
dhuby <2010024057@ust-ics.mygbiz.com>
2015-11-24 18:40:21 +0800

    deleted

--- application/views/includes/memberTemplate.php
+++
@@ -1,7 +0,0 @@
-<?php $this->load->view('includes/memberHeader') ;?>
-
-<?php $this->load->view('includes/memberNav'); ?>
-
-<?php $this->load->view($main_content); ?>
-
-<?php $this->load->view('includes/memberFooter'); ?>
